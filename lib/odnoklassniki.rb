45728a233375a1a4c5695ef4a92500eddbb0f0d9
Alexey Gaziev <alex.gaziev@gmail.com>
2015-02-03 14:55:53 +0800

    Start rewriting client with weary

---
+++ lib/odnoklassniki.rb
@@ -0,0 +1,6 @@
+require 'odnoklassniki/error'
+require 'odnoklassniki/version'
+
+module Odnoklassniki
+  require 'odnoklassniki/client'
+end
--- lib/odnoklassniki/client.rb
+++ lib/odnoklassniki/client.rb
@@ -1,58 +1,29 @@
-require 'odnoklassniki/error'
-require 'odnoklassniki/version'
-
-require 'multi_json'
 require 'digest/md5'
 
 module Odnoklassniki
-  class Client
-    attr_accessor :access_token, :refresh_token,
-                  :application_id, :application_key, :application_secret
-    attr_writer :user_agent
+  class Client < Weary::Client
+    OAUTH_PARAM_NAMES = [:token,
+                         :refresh_token,
+                         :client_id,
+                         :client_secret,
+                         :application_key].freeze
 
-    # Initializes a new Client object
-    #
-    # @param options [Hash]
-    # @return [Odnoklassniki::Client]
-    def initialize(options = {})
-      options.each do |key, value|
-        instance_variable_set("@#{key}", value)
+    domain 'http://api.odnoklassniki.ru/fb.do'
+
+    user_agent "Odnoklassniki API Client (Ruby)/#{Odnoklassniki::VERSION} (+http://github.com/gazay/odnoklassniki)"
+
+    def initialize(oauth_params = {})
+      @defaults = {}
+      OAUTH_PARAM_NAMES.each do |param|
+        @defaults[param] = hash_fetch(params, param)
       end
-      yield(self) if block_given?
     end
 
-    # @return [Boolean]
-    def user_token?
-      !!(access_token && refresh_token)
+    private
+
+    def hash_fetch(hash, key)
+      hash[key] || hash[key.to_s]
     end
 
-    # @return [String]
-    def user_agent
-      @user_agent ||= "OdnoklassnikiRubyGem/#{Odnoklassniki::Version}"
-    end
-
-    # @return [Hash]
-    def credentials
-      {
-        token: access_token,
-        refresh_token: refresh_token,
-        client_id: appication_id,
-        client_secret: application_secret,
-        application_key: application_key
-      }
-    end
-
-    # @return [Boolean]
-    def credentials?
-      credentials.values.all?
-    end
-
-    # @return [String]
-    def signature
-    end
-
-    # @return [String]
-    def renew_token
-    end
   end
 end
